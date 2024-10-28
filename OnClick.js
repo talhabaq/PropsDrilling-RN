0853d7be132168380017fb4518e9eab60225b2f3
TALHA BAQAR HUSSAIN <159868699+talhabaq@users.noreply.github.com>
2024-10-28 17:28:15 +0500

    Create OnClick.js

---
+++ OnClick.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import OnMouseOver from './OnMouseOver';
+
+const OnClick = () => {
+  return (
+    <View >
+     <OnMouseOver/>
+    </View>
+  );
+};
+
+
+
+export default OnClick;
